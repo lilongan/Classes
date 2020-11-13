01ccb3d493b71b2a9eba26423077b334badd4400
Art Lees <art.lees@dragonpeace.com>
2020-11-13 06:39:04 -0500

    201113 AM1 push

---
+++ Wk04'py'html'css'js/201103'py'js/Practice/X5.js
@@ -0,0 +1,17 @@
+let myName = "RT";
+let myAge = 64;
+let myHeight = 71;
+let myWeight = 185;
+let myEyes = "brown";
+let myTeeth = "white";
+let myHair = "brown";
+
+console.log(`Let's talk about ${myName}.`)
+console.log(`He's ${myHeight} inches tall.`)
+console.log(`He's ${myWeight} pounds heavy.`)
+console.log(`Actually, that's not too heavy.`)
+console.log(`He's got ${myEyes} eyes and ${myHair} hair.`)
+console.log(`His teeth are usually ${myTeeth} depending on the coffee.`)
+
+let total = myAge + myHeight + myWeight;
+console.log(`If I add ${myAge}, ${myHeight}, and ${myWeight}, I get ${total}.`)
--- Wk04'py'html'css'js/201103'py'js/Practice/X5.py
+++ Wk04'py'html'css'js/201103'py'js/Practice/X5.py
@@ -13,6 +13,7 @@ print("Actually, that's not too heavy.")
 print(f"He's got {my_eyes} eyes and {my_hair} hair.")
 print(f"His teeth are usually {my_teeth} depending on the coffee.")
 
-
 total = my_age + my_height + my_weight
 print(f"If I add {my_age}, {my_height}, and {my_weight}, I get {total}.")
+
+# Terminal cmd python3 X5.py
